6a359999fae730347857d25472af4c642da9d8c3
julianegutierrez <gutierrezjuliane@gmail.com>
2025-09-27 11:44:16 +0800

    removed heroku repo name from settings

--- crowndfunding/crowndfunding/urls.py
+++ crowndfunding/crowndfunding/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fundraisers.urls')),
     path('', include('users.urls')),
-    path('', include('juliane-gutierrez-she-codes-django.urls')),
     path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth')
 
 ]
